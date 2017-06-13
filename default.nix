6ccea1c3fd4cc1ec378988eafd2090d8987c0236
Josef Kemetmueller <josef.kemetmueller@gmail.com>
2017-06-13 18:55:48 +0200

    Add sandman2

---
+++ default.nix
@@ -0,0 +1,63 @@
+self: super: with self; {
+  python = super.python.override {
+    packageOverrides = python-self: python-super: {
+      pytest-flask = pythonPackages.buildPythonPackage rec {
+        name = "pytest-flask-${version}";
+        version = "0.10.0";        src = pkgs.fetchurl {
+          url = "mirror://pypi/p/pytest-flask/${name}.tar.gz";
+          sha256 = "00vznyjfn6qszq6v551z53y9g4gqsjysxvyvbpwbdy1y0gwkcnic";
+        };
+        propagatedBuildInputs = with pythonPackages; [
+          pytest
+          werkzeug
+          flask
+        ];
+      };
+      flask-httpauth = pythonPackages.buildPythonPackage rec {
+        name = "Flask-HTTPAuth-${version}";
+        version = "3.2.3";
+        src = pkgs.fetchurl {
+          url = "mirror://pypi/f/flask-httpauth/${name}.tar.gz";
+          sha256 = "13gff5w1mqpzm5nccyg02v3ifb9ifqh5k866cssjhghhg6msfjsz";
+        };
+        propagatedBuildInputs = with pythonPackages; [
+          flask
+        ];
+      };
+      flask-admin = pythonPackages.buildPythonPackage rec {
+        name = "Flask-Admin-${version}";
+        version = "1.5.0";
+        src = pkgs.fetchurl {
+          url = "mirror://pypi/f/flask-admin/${name}.tar.gz";
+          sha256 = "1iabc9x540i1wgb97nr5xxrnblzzdkg69lw3gv6r7qi2kzcrcfpy";
+        };
+        propagatedBuildInputs = with pythonPackages; [
+          flask
+          wtforms
+        ];
+        doCheck = false;
+      };
+      sandman2 = pythonPackages.buildPythonPackage rec {
+        name = "sandman2-${version}";
+        version = "1.0.6";
+        src = pkgs.fetchurl {
+          url = "mirror://pypi/s/sandman2/${name}.tar.gz";
+          sha256 = "0dk62iqkrphmf9c8vcjg5y1wcc3qqwnrbxl5bswknfmr453ncd7y";
+        };
+        patchPhase = ''
+          substituteInPlace setup.py --replace "SQLAlchemy==1.1.0b3" "SQLAlchemy>=1.1.0b3"
+        '';
+        propagatedBuildInputs = with pythonPackages; [
+          pytest-flask
+          flask-httpauth
+          flask-admin
+          sqlalchemy
+          flask_sqlalchemy
+          pytestcov
+        ];
+
+      };
+    };
+  };
+  pythonPackages = python.pkgs;
+}
